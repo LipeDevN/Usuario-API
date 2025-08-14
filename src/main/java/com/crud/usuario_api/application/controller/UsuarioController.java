51e9456925a622754a4e49f5ec2a576cb6ec919e
Your Name <you@example.com>
2025-08-14 02:43:26 -0300

    Feat: Criação de um Controller com os métodos: create, listAll, findById, DeleteById, UpdateById

--- src/main/java/com/crud/usuario_api/application/controller/UsuarioController.java
+++ src/main/java/com/crud/usuario_api/application/controller/UsuarioController.java
@@ -1,9 +1,50 @@
 package com.crud.usuario_api.application.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.crud.usuario_api.application.dto.UsuarioRequestDTO;
+import com.crud.usuario_api.application.dto.UsuarioResponseDTO;
+import com.crud.usuario_api.application.service.UsuarioService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
+
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponseDTO> criarUsuario(@RequestBody UsuarioRequestDTO requestDTO){
+        UsuarioResponseDTO responseDTO = usuarioService.createUsuario(requestDTO);
+        return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDTO>> listarUsuario() {
+        return ResponseEntity.ok(usuarioService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> buscarUsuarioPorId(@PathVariable Long id){
+        UsuarioResponseDTO responseDTO = usuarioService.findUsuarioById(id);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> atualizarUsuario(@PathVariable Long id, @RequestBody UsuarioRequestDTO requestDTO) {
+        UsuarioResponseDTO dto = usuarioService.atualizarUsuario(id, requestDTO);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+        usuarioService.deleteUsuario(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
